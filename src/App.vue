07df54cecd372614c2e3f52257d94f43b7cc3167
Felipe <felipeeduardofreitasm@gmail.com>
2026-01-26 21:39:46 -0300

    initial(board and timer): criacao da estrutura do jogo, do tabuleiro e do contator de tempo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<style>
+.game-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 720px;
+    width: 720px;
+    border-radius: 20px;
+    background-color: red;
+}
+
+</style>
+
+<script setup>
+import Timer from './components/Timer.vue';
+import Board from './components/Board.vue';
+
+</script>
+
+<template>
+    <div class="game-container">
+        <Board />
+        <Timer />
+    </div>
+</template>
---
+++ src/components/Timer.vue
@@ -0,0 +1,22 @@
+<style>
+.timer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: green;
+    height: 80px;
+    width: 160px;
+}
+</style>
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="timer">
+        <span class="time">
+            00:01
+        </span>
+    </div>
+</template>
