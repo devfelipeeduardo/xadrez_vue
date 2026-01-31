5386a16210ec3e4cd1c4aeac33cace9e692d5041
Felipe <felipeeduardofreitasm@gmail.com>
2026-01-31 15:36:27 -0300

    feature: Ao selecionar uma peca, altera o fundo. Inicio da implementacao da movimentacao de peoes

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -27,6 +27,14 @@
 .piece {
     width: 80%;
     height: 80%;
+    border-radius: 5px;
+}
+.piece:hover {
+  cursor: pointer;
+}
+
+.selected-color {
+  background-color: rgba(33, 29, 160, 0.414);
 }
 </style>
 
@@ -45,14 +53,77 @@ export default {
                     ['', '', '', '', '', '', '', ''],
                     ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
                     ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
-                ]
+                ],
+            selectedPiece: {
+              row: null,
+              col: null,
+              piece: null
+            },
+            selectedTarget: {
+              row: null,
+              col: null
+            }
         }
     },
     methods: {
-        getPieceImage(piece){
-            if (!piece) return null
-            return new URL(`../assets/pieces_image/${piece}.png`, import.meta.url).href
+      getPieceImage(piece){
+          if (!piece) return null
+          return new URL(`../assets/pieces_image/${piece}.png`, import.meta.url).href
+      },
+
+      setSelectedPiece(piece, rowIndex, columIndex) {
+
+        this.selectedPiece.piece = piece;
+        this.selectedPiece.row = rowIndex;
+        this.selectedPiece.col = columIndex;
+      },
+
+      isPieceSelected(rowIndex, columIndex) {
+        return (
+          this.selectedPiece.row === rowIndex 
+          &&
+          this.selectedPiece.col === columIndex
+        );
+      },
+
+      setSelectedTarget(rowIndex, columnIndex) {
+        if (rowIndex == null || columnIndex == null) {
+          this.selectedPiece.piece = null;
+          this.selectedPiece.row = null;
+          this.selectedPiece.col = null;
         }
+        this.selectedTarget.row = rowIndex;
+        this.selectedTarget.column = columnIndex;
+
+        movePiece(piece)
+      },
+
+      movePiece(piece) {
+        if (piece[1] === 'P') {
+          movePawn(actualRowPosition, actualColumnPosition)
+        }
+      },
+
+      movePawn(actualRowPosition, actualColumnPosition) {
+        const MOVE_FOWARD = 1;
+        const MOVE_BACKWARD = -1;
+        const INITIAL_LIMIT_BOARD = 0;
+        const FINAL_LIMIT_BOARD = 7;
+
+        const piece = this.board[actualRowPosition][actualColumPosition];
+        if (!piece || piece[1] !== 'P') return;
+        const color = piece[0];
+
+        const direction = color === 'w' ? MOVE_FOWARD : MOVE_BACKWARD;
+        const newRow = actualRowPosition + direction;
+
+        if (newRow < INITIAL_LIMIT_BOARD || newRow > FINAL_LIMIT_BOARD) return;
+        if (this.board[newRow][actualColumPosition] !== '') return;
+
+        // Movimentação:
+        this.board[newRow][actualColumPosition] = piece;
+        this.board[actualRowPosition][actualColumPosition] = '';
+      }
     }
 }
 </script>
@@ -61,16 +132,25 @@ export default {
 <template>
   <div class="board">
     <span
-      v-for="(cell, index) in board.flat()"
-      :key="index"
-      class="position"
-      :class="(Math.floor(index / 8) + index % 8) % 2 === 0 ? 'light' : 'dark'"
-    >
-      <img
-        v-if="cell"
-        :src="getPieceImage(cell)"
-        class="piece"
-      />
+      v-for="(row, rowIndex) in board"
+      :key="rowIndex"
+      style="display: contents"
+      >
+      <span
+        v-for="(piece, colIndex) in row"
+        :key="`${rowIndex}-${colIndex}`"
+        class="position"
+        :class="(rowIndex + colIndex) % 2 === 0 ? 'light' : 'dark'"
+        @click="setSelectedTarget(rowIndex, colIndex)"
+        >
+        <img
+          v-if="piece"
+          :src="getPieceImage(piece)"
+          class="piece"
+          :class="isPieceSelected(rowIndex, colIndex) ? 'selected-color' : ''"
+          @click="setSelectedPiece(piece, rowIndex, colIndex)"
+        />
+      </span>
     </span>
   </div>
 </template>
